4f765271267f47bb2fa7a8445344ec1d6c7bdd86
David Ford <djfordz@gmail.com>
2018-07-01 02:26:51 -0500

    remove html in admin labels since Magento now escapes them

--- app/code/local/Filofax/BulkUpload/Model/BulkImageUpload.php
+++ app/code/local/Filofax/BulkUpload/Model/BulkImageUpload.php
@@ -25,43 +25,93 @@ class Filofax_BulkUpload_Model_BulkImageUpload extends Mage_Core_Model_Abstract
                 $paths = $this->getImagePath($sku);
                 $i = 0;
                 if (count($paths) > 0) {
+                    $skuPaths = array();
                     foreach ($paths as $path) {
-                        $img = $this->splitArray($path);
-                        if (file_exists($img['path'])) {
-                            $product->addImageToMediaGallery($img['path'], $img['options'], true, false);
-                        } 
+                        
+                        $pathParts = pathinfo($path);
+                        preg_match('/([-\d\w]*)__/', $pathParts['filename'], $skuPaths);
+                        if ($skuPaths[1] == $sku) {
+                            $img = $this->splitArray($path);
+                            $parentProduct = $this->getParentProduct($product);
+                            if (isset($parentProduct) && $img['front'] == true) {
+                                if ($img['config_value'] == true) {
+                                    $parentTypes = array('image', 'small_image', 'thumbnail');
+                                    $parentSort = 0;
+                                } else {
+                                    $parentTypes = array();
+                                    if ($i == 0) {
+                                        $parentSort = 1;
+                                    } else {
+                                        $parentSort = $i;
+                                    }
+                                    
+                                }
+                                $path_parts = pathinfo($img['path']);
+                                $parentImgPath = $path_parts['dirname'] . '/' . $path_parts['filename'] . '-1.' . $path_parts['extension'];
+                                $this->addImage($parentProduct, $parentImgPath, $parentTypes, $img['label'], $parentSort);
+                                
+                            }
+                            $this->addImage($product, $img['path'], $img['types'], $img['label'], $img['sort_order']);
+                            $i++;
+                            $parentProduct->save();
+                        }
                     }
                     
-
-                    if (!empty($img['label'])) {
-                        $this->addLabel($product, $img['label']);
+                    if ($skuPaths[1] == $sku) {
+                        $product->save();
                     }
-
-                    $product->save();
+                    
                 }
             }
         }
         echo json_encode(array("response" => "true"));
     }
 
-    protected function addLabel($product, $label) {
-        foreach ($product->getData('media_gallery') as $each) {
-            foreach ($each as $image) {
-                $attributes = $product
-                    ->getTypeInstance(true)
-                    ->getSetAttributes($product);
-                $attributes['media_gallery']
-                    ->getBackend()
-                    ->updateImage($product, $image['file'], array('label' => $label));
+    protected function getParentProduct($product)
+    {
+        $parent = null;
+        if ($product->getTypeId() == "simple") {
+            $parentIds = Mage::getModel('catalog/product_type_grouped')
+                ->getParentIdsByChild($product->getId());
+            if (!$parentIds) {
+                $parentIds = Mage::getModel('catalog/product_type_configurable')
+                    ->getParentIdsByChild($product->getId());
+            }
+
+            if (isset($parentIds[0])) {
+                $parent = Mage::getModel('catalog/product')->load($parentIds[0]);
             }
         }
+        
+        return $parent;
+    }
 
+    protected function addImage($product, $file, $types, $label, $sortOrder) {
+
+        try {
+            $attributes = $product
+                ->getTypeInstance(true)
+                ->getSetAttributes($product);
+            $filename = $attributes['media_gallery']
+                ->getBackend()
+                ->addImage($product, $file, $types, true, false);
+            $attributes['media_gallery']
+                ->getBackend()
+                ->updateImage($product, $filename, array('label' => $label, 'position' => $sortOrder));
+        } catch (Exception $e) {
+            Mage::logException($e);
+        }
+        
     }
 
     protected function splitArray($var)
     {
-        $options = array();
+        $types = array();
         $label = null;
+        $sortOrder = null;
+        $order = null;;
+        $configVal = null;
+        $front = null;
 
         if (empty($var)) {
             return;
@@ -70,25 +120,73 @@ class Filofax_BulkUpload_Model_BulkImageUpload extends Mage_Core_Model_Abstract
         if (is_array($var)) {
             $this->splitArray($var);
         } else {
-            preg_match('/(_\d)(_\d)(_\d)(?:(_\w*))/', $var, $imgOptions);
+
+            preg_match_all('/__([a-zA-Z-1]+)?_?([a-zA-Z]+[\ -]?[a-zA-Z]+[-\ \:]?[a-zA-Z]+)?_?([\d])?_?([\d])?_?([\d])?_?([\d])?/', $var, $imgOptions);
 
             if (count($imgOptions) > 0) {
-                for($i = 0; $i <= count($imgOptions); $i++) {
+                for ($i = 0; $i <= count($imgOptions) - 1; $i++) {
                     switch ($i) {
-                        case 1: $imgOptions[$i] == '_1' ? array_push($options, 'image') : array_push($options, '');
+                    case 1: $order = isset($imgOptions[$i][0]) ? $imgOptions[$i][0] : '';
                         break;
-                        case 2: $imgOptions[$i] == '_1' ? array_push($options, 'small_image') : array_push($options, '');
+                    case 2: $label = isset($imgOptions[$i][0]) ? $imgOptions[$i][0] : '';
                         break;
-                        case 3: $imgOptions[$i] == '_1' ? array_push($options, 'thumbnail') : array_push($options, '');
+                    case 3: $sortOrder = isset($imgOptions[$i][0]) ? substr($imgOptions[$i][0], 1) : '';
                         break;
-                        case 4: $label = isset($imgOptions[$i]) ? substr($imgOptions[$i], 1) : null;
+                    case 4: $imgOptions[$i][0] == '1' ? array_push($types, 'image') : array_push($types, '');
+                        break;
+                    case 5: $imgOptions[$i][0] == '1' ? array_push($types, 'small_image') : array_push($types, '');
+                        break;
+                    case 6: $imgOptions[$i][0] == '1' ? array_push($types, 'thumbnail') : array_push($types, '');
                         break;
                     }
                 }
             }
 
-            return array('path' => $var, 'options' => $options, 'label' => $label);
+            if (!empty($order)) {
+                $orderArr = $this->_checkOrder($order);
+                $sortOrder = $orderArr['sort_order'];
+                $configVal = $orderArr['config_value'];
+                $front = $orderArr['front'];
+                $types = $orderArr['types'];
+                if ($front) {
+                    $pathParts = pathinfo($var);
+                    if (!copy($var, $pathParts['dirname'] . '/' . $pathParts['filename'] . '-1.' . $pathParts['extension'])) {
+                        Mage::log('failed to copy file', null, 'test.log');
+                    }
+                }
+            }
+
+            if (strpos($label, ':')) {
+                $label = str_replace(':', '/', $label);
+            }
+
+            return array('path' => $var, 'types' => $types, 'label' => $label, 'sort_order' => $sortOrder, 'config_value' => $configVal, 'front' => $front);
         }
+    }
+
+    protected function _checkOrder($name)
+    {
+        $sortOrder = '';
+        $configVal = false;
+        $front = false;
+        $types = null;
+
+        $name = strtolower($name);
+
+        switch ($name) {
+            case 'front': $sortOrder = 0; $front = true; $types = array('image', 'small_image', 'thumbnail');
+                break;
+            case 'iso': $sortOrder = 1;
+                break;
+            case 'open': $sortOrder = 2;
+                break;
+            case 'inside' : $sortOrder = 3;
+                break;
+            case 'front-1' : $sortOrder = 0; $configVal = true; $front = true; $types = array('image', 'small_image', 'thumbnail');
+                break;
+        }
+
+        return array('sort_order' => $sortOrder, 'config_value' => $configVal, 'front' => $front, 'types' => $types);
     }
 
     protected function getImagePath($pattern) {
--- app/code/local/Filofax/BulkUpload/controllers/Adminhtml/BulkImport/BulkImageImportController.php
+++ app/code/local/Filofax/BulkUpload/controllers/Adminhtml/BulkImport/BulkImageImportController.php
@@ -28,21 +28,25 @@ class Filofax_BulkUpload_Adminhtml_BulkImport_BulkImageImportController extends 
                 A specific image naming convention must be adhered to.</h3>
                 <p>The convention allows for the module to search for the product by sku and add the image to the specified product.</p>
                  <p>All images must be uploaded to <pre>[mageroot]/media/import/</pre> directory prior to running the import process.</p>
-                <p>All image must start with product sku to be attached to and select either 1 or 0 with an undersore between each value for <pre>base_image, small_image, thumbnail</pre></p>
-                <p>An example of an image file name is below: <br/>
-                <pre>20-68208_1_1_1.jpg</pre>
+                <p>All image must start with product sku to be attached to followed by a __ and the label next, then the sort_order, then select either 1 or 0 with an undersore between each value for <pre>base_image, small_image, thumbnail</pre></p>
+                <p>The image naming convention is as follows:
+                <pre>[sku]__[location]_[label]_[sort_order]_[base_image]_[small_image]_[thumbnail].[jpg|png|gif]</pre>
                 or
-                <pre>19-65400_0_0_0.jpg</pre>
-                or<br />
-                the image can have just the sku as a name like
-                <pre>186534.jpg</pre>
-                this configuration assumes there are no options for base_image, small_image, and thumbnail.</p>
-                <p>Other configurations are also compatible as long as the sku is the first part of the name followed by _<br />
-                and as long as no options are needed. So
-                <pre>18-206820_large.jpg</pre>
-                would be valid as well.</p>
-                <p>If a label needs to be added, the underscores are mandatory and the label will be the last part of the name.<br />example:
-                <pre>19-18745_0_0_0_Purple.jpg</pre></p>
+                <pre>[sku]__[location]_[color].[jpg|png|gif]</pre>
+                location must be set, if manually setting sort order, use an arbitrary letter or combination of letters for location.<br />
+                <p>An example of an image file name is below: <br/>
+                <pre>20-68208___t_Memo-Black-Red_0_1_1_1.jpg</pre>
+                or
+                <pre>19-65400__front_Black.jpg</pre></p>
+                <p>using front, iso, inside, open, as sort order. is preferred.
+                <pre>[SKU]__front_Memo Black:Red.jpg</pre>
+                also the program will automatically set the configurable product images if front is set for simple product.<br />
+                if you want to set the image as base image, small image, and thumbnail image for the config use \'front-1\'<br />
+                <pre>19-88880__front-1_Black.jpg</pre>
+                <pre>19-88880__iso_Black.jpg</pre>
+                <pre>19-88880__open_Black.jpg</pre>
+                <pre>19-88880__inside_Black.jpg</pre>
+                will set \'front-1\' for configurable and set the others with proper sort order for simple</p>
                 <p>After all images are uploaded via ftp to [mageroot]/media/import/</p>
                 <p>To Run, select the button below and the importer will run, parsing all images.</p>'
             );
